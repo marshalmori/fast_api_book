68ea4a9e812079d012ac4ea5dd19c0695c9dff08
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-05-02 15:54:05 -0300

    Pydantic Book Request Validation

--- book.py
+++ book.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,6 +19,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
@@ -34,5 +43,6 @@ async def read_all_books():
 
 
 @app.post("/create-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(new_book)
