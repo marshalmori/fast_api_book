a1c7ecec8995076634f3334bd3141efd0d8b3a05
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-05-07 14:51:34 -0300

    Data Validation Path Parameters

--- book.py
+++ book.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -58,7 +58,7 @@ async def read_all_books():
 
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -106,7 +106,7 @@ async def update_book(book: BookRequest):
 
 
 @app.delete('/book/{book_id}')
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
